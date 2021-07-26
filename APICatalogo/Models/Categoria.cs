a97b2fdc78880ef663e9ab392f6ceac8858446b7
IlkaCarmo <ilka_2009@ig.com.br>
2021-07-26 20:32:00 -0300

    Projeto iniciado

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
+
+    }
+}
